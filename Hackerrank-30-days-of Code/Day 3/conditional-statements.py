5bcb196369df97d1b95b896e1dcd977da72dade1
Pradeep Singh <pdeepsingh91@gmail.com>
2018-09-28 11:20:55 -0700

    Update conditional-statements.py

--- Hackerrank-30-days-of Code/Day 3/conditional-statements.py
+++ Hackerrank-30-days-of Code/Day 3/conditional-statements.py
@@ -1,1 +1,19 @@
-hi
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+
+if n%2 != 0:
+    print('Weird')
+elif n%2 == 0 and n < 6 and n > 1:
+    print('Not Weird')
+elif n%2 == 0 and n > 5 and n < 21:
+    print('Weird')
+elif n%2 == 0 and n > 20:
+    print('Not Weird')
