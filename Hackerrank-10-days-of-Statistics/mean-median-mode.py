46a33d48c66f166c7bdddbc31a0620c1876cc6d7
Pradeep Singh <pdeepsingh91@gmail.com>
2018-10-04 10:55:01 -0700

    Update mean-median-mode.py

--- Hackerrank-10-days-of-Statistics/mean-median-mode.py
+++ Hackerrank-10-days-of-Statistics/mean-median-mode.py
@@ -1,1 +1,11 @@
-hi
+# Find Mean, Median and Mode
+
+import numpy as np 
+from scipy import stats
+
+n = int(input())
+x = list(map(int,input().split()))
+
+print(np.mean(x))
+print(np.median(x))
+print(int(stats.mode(x)[0]))
