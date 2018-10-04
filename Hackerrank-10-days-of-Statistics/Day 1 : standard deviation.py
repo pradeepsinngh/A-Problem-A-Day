117bc70fbd68c3afee56d4d8c716a747df1df96b
Pradeep Singh <pdeepsingh91@gmail.com>
2018-10-04 11:47:06 -0700

    Update Day 1 : standard deviation.py

--- Hackerrank-10-days-of-Statistics/Day 1 : standard deviation.py
+++ Hackerrank-10-days-of-Statistics/Day 1 : standard deviation.py
@@ -1,1 +1,9 @@
 # find std. deviation
+
+n = int(input())
+y = list(map(int, input().split()))
+
+mean = sum(y)/n
+variance = sum([((x-mean) ** 2) for x in y]) /n
+stddev = variance ** 0.5
+print(round(stddev,1))
