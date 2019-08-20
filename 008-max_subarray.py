64a52733bd1da6f67bc986bb0847231d8d576803
Pradeep Singh <pdeepsingh91@gmail.com>
2019-08-19 21:07:39 -0400

    Update 008-max_subarray.py

--- 008-max_subarray.py
+++ 008-max_subarray.py
@@ -1,4 +1,5 @@
-# Prob: Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+# Prob: Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest 
+# sum and return its sum.
 
 class Solution(object):
     def maxSubArray(self, nums):
