d8ae950f5e596b1f9bec62c90e704bef295a232b
Pradeep Singh <pdeepsingh91@gmail.com>
2019-08-26 19:23:05 -0400

    Update 002-defanging_IP_address.py

--- by-data-structure/strings/easy/002-defanging_IP_address.py
+++ by-data-structure/strings/easy/002-defanging_IP_address.py
@@ -5,9 +5,16 @@
 
 # Explaination:
 
-# Some may be quick to write a quick replace call on the string for a one line solution, but it's important to examine how this will work. In python, strings are immutable objects, meaning if we are searching for '.', we will have as many stack calls underneath the surface when we replace as the number of occurences of '.' in the string.
-# Thus, since we cannot modify a string in place like we can an array, and since for a general replace call we could have k instances of '.', where k<=n, the worst-case time complexity for a replace call is actually O(n*k)=O(n*n) where n is the length of the string.
-# A better approach would be to merely split the string up into an array each time we see a . , then join these elements with the proper '[.]' string. With the split and join functions, we parse the input twice, once as the original string, and once as the split array, however, this equates to a time complexity of O(2*n)=O(n).
+# Some may be quick to write a quick replace call on the string for a one line solution, but it's important to examine how 
+# this will work. In python, strings are immutable objects, meaning if we are searching for '.', we will have as many stack 
+# calls underneath the surface when we replace as the number of occurences of '.' in the string.
+
+# Thus, since we cannot modify a string in place like we can an array, and since for a general replace call we could have k 
+# instances of '.', where k<=n, the worst-case time complexity for a replace call is actually O(n*k)=O(n*n) where n is the 
+# length of the string.
+# A better approach would be to merely split the string up into an array each time we see a . , then join these elements with t
+# he proper '[.]' string. With the split and join functions, we parse the input twice, once as the original string, and once 
+# as the split array, however, this equates to a time complexity of O(2*n)=O(n).
 
 
 
