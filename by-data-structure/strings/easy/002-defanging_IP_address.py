d6276f8d4af78422d247ff07a7249aee8750b5f5
Pradeep Singh <pdeepsingh91@gmail.com>
2019-08-26 18:50:41 -0400

    Create 002-defanging_IP_address.py

---
+++ by-data-structure/strings/easy/002-defanging_IP_address.py
@@ -0,0 +1,13 @@
+# Prob: Defanging an IP Address
+# Given a valid (IPv4) IP address, return a defanged version of that IP address.
+# A defanged IP address replaces every period "." with "[.]".
+
+# Sol1: 
+class Solution(object):
+    def defangIPaddr(self, address):
+        """
+        :type address: str
+        :rtype: str
+        """
+       
+        return address.replace('.', '[.]')
