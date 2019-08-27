4dfe52314a1cca195ccb4d078601e8344c7406a4
Pradeep Singh <pdeepsingh91@gmail.com>
2019-08-26 20:15:49 -0400

    Create 001-multiply_strings.py

---
+++ by-data-structure/strings/medium/001-multiply_strings.py
@@ -0,0 +1,11 @@
+# Prob: Multiply Strings
+
+# Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also 
+# represented as a string.
+
+# Sol1: 
+def multiply1(self, num1, num2):
+    return str(int(num1)*int(num2))
+    
+
+# Sol2:
