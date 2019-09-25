660308a0575d0dbab011ddc9200d71ddddbe4d36
Pradeep Singh <pdeepsingh91@gmail.com>
2019-09-24 22:34:08 -0400

    Create 006-implement_strStr().py

---
+++ by-data-structure/strings/easy/006-implement_strStr().py
@@ -0,0 +1,36 @@
+```
+# Implement strStr().
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+```
+
+# Sol1: 
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
+        for i in range(len(haystack) - len(needle)+1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        
+        return -1
+        
+        
+# Sol2:
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        if needle not in haystack:
+            return -1
+        else:
+            return haystack.index(needle)
