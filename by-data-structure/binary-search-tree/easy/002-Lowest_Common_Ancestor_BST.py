9c1bd4390366e165e1260c44f32567eabcd98398
Pradeep Singh <pdeepsingh91@gmail.com>
2020-02-19 00:35:37 -0800

    Create 002-Lowest_Common_Ancestor_BST.py

---
+++ by-data-structure/binary-search-tree/easy/002-Lowest_Common_Ancestor_BST.py
@@ -0,0 +1,26 @@
+# Prob: Lowest Common Ancestor of a Binary Search Tree
+# Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return root
+        
