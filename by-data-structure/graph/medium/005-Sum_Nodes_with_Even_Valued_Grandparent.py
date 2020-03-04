0cb6602fcea3b6b70d5a9a3dd205d5bf6c7d89d1
Pradeep Singh <pdeepsingh91@gmail.com>
2020-03-04 15:15:55 -0800

    Update 005-Sum_Nodes_with_Even_Valued_Grandparent.py

--- by-data-structure/graph/medium/005-Sum_Nodes_with_Even_Valued_Grandparent.py
+++ by-data-structure/graph/medium/005-Sum_Nodes_with_Even_Valued_Grandparent.py
@@ -1,6 +1,13 @@
 Prob: Sum of Nodes with Even-Valued Grandparent
 https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/
 
+```
+Given a binary tree, return the sum of values of nodes with even-valued grandparent.  (A grandparent of a node is the parent of its parent, if it exists.)
+
+If there are no nodes with an even-valued grandparent, return 0.
+```
+    
+    
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
