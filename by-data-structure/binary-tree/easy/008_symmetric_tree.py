7b086d2a10b6062515ffea8916200b29ae6cc1b3
Pradeep Singh <pdeepsingh91@gmail.com>
2020-04-13 14:18:34 -0700

    Create 008_symmetric_tree.py

---
+++ by-data-structure/binary-tree/easy/008_symmetric_tree.py
@@ -0,0 +1,37 @@
+```
+# Prob: Symmetric Tree:
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+```
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        
+        if root is None:
+            return True
+        else:
+            return self.isMirror(root.left, root.right)
+        
+    def isMirror(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        
+        if left.val == right.val:
+            outPair = self.isMirror(left.left, right.right)
+            inPair = self.isMirror(left.right, right.left)
+            return outPair and inPair
+        else:
+            return False
