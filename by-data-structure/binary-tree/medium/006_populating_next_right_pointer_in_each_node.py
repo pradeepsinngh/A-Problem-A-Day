3ae1afba6acf26f191b84714bad45b6d24536b27
Pradeep Singh <pdeepsingh91@gmail.com>
2020-04-13 17:33:29 -0700

    Update 006_populating_next_right_pointer_in_each_node.py

--- by-data-structure/binary-tree/medium/006_populating_next_right_pointer_in_each_node.py
+++ by-data-structure/binary-tree/medium/006_populating_next_right_pointer_in_each_node.py
@@ -9,7 +9,7 @@ Initially, all next pointers are set to NULL.
 '''
 
 ## -----------------------
-# Sol 1: 
+# Sol 1: Using Recursive Approach
 
 # Definition for a Node.
 class Node(object):
@@ -39,5 +39,73 @@ class Solution(object):
 
 
 ## -----------------------
-# Sol 2:
+# Sol 2: DFS (Using stack
 
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        # DFS (using stack)
+        if not root:
+            return
+        
+        stack = [root]
+        
+        while stack:
+            curr = stack.pop()
+            if curr.left and curr.right:
+                curr.left.next = curr.right
+                if curr.next:
+                    curr.right.next = curr.next.left
+                stack.append(curr.right)
+                stack.append(curr.left)
+        return root
+        
+        
+### ------------    
+# Sol 3: BFS (using queue)
+
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=0, left=None, right=None, next=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution(object):
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        # BFS (using stack)
+        
+        if not root:
+            return
+        
+        q = [root]
+        
+        while q:
+            curr = q.pop(0)
+            if curr.left and curr.right:
+                curr.left.next = curr.right
+                if curr.next:
+                    curr.right.next = curr.next.left
+                q.append(curr.left)
+                q.append(curr.right)
+                
+        return root
+        
