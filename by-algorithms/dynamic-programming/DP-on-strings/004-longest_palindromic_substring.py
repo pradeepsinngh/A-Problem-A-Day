3113d46f220a7ade2fe0695f49f05c6d3d5b9678
Pradeep Singh <pdeepsingh91@gmail.com>
2020-04-23 15:52:34 -0700

    Create 004-longest_palindromic_substring.py

---
+++ by-algorithms/dynamic-programming/DP-on-strings/004-longest_palindromic_substring.py
@@ -0,0 +1,13 @@
+'''
+Pron : Longest Palindromic Substring
+
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+Input: "babad"
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+
+'''
+
