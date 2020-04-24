6add6b4bccd07c379a713ce116b6e92f5e643a7e
Pradeep Singh <pdeepsingh91@gmail.com>
2020-04-24 00:52:55 -0700

    Create 004-best_time_buy_sell_stock_transaction_fee.py

---
+++ by-algorithms/dynamic-programming/decision-making/004-best_time_buy_sell_stock_transaction_fee.py
@@ -0,0 +1,29 @@
+'''
+Best Time to Buy and Sell Stock with Transaction Fee
+
+Your are given an array of integers prices, for which the i-th element is the price of a given stock on day i; 
+and a non-negative integer fee representing a transaction fee.
+
+You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction. 
+You may not buy more than 1 share of a stock at a time (ie. you must sell the stock share before you buy again.)
+
+Return the maximum profit you can make.
+
+'''
+
+class Solution(object):
+    def maxProfit(self, prices, fee):
+        """
+        :type prices: List[int]
+        :type fee: int
+        :rtype: int
+        """
+        T_ik0 = 0
+        T_ik1 = float('-inf')
+    
+        for price in prices:
+            T_ik0_old = T_ik0
+            T_ik0 = max(T_ik0, T_ik1 + price)
+            T_ik1 = max(T_ik1, T_ik0_old - price - fee)
+        
+        return T_ik0
