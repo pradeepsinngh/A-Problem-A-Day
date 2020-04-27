2e047bb403172563f011d8172baa5cf6481b3fc1
Pradeep Singh <pdeepsingh91@gmail.com>
2020-04-27 12:24:42 -0700

    Update merge_k_sorted_list.py

--- by-data-structure/linked-list/merge_k_sorted_list.py
+++ by-data-structure/linked-list/merge_k_sorted_list.py
@@ -44,3 +44,39 @@ class Solution(object):
         
         return start.next
         
+# Approach 2:
+# Time - O(N log K) k is number of list
+# Space - O(1)
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        heap = []
+        for idx, list in enumerate(lists):
+            if list:
+                heap.append((list.val, idx))
+        
+        heapq.heapify(heap)
+        
+        curr = head = ListNode(None)
+        while heap:
+            val, idx = heapq.heappop(heap)
+            curr.next = ListNode(val)
+            curr = curr.next
+            
+            node = lists[idx] = lists[idx].next
+            if node:
+                heapq.heappush(heap, (node.val, idx))
+        return head.next
+                
+        
