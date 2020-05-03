7ce5f84b41ad6b193376ac977bc04d3fa493332f
Pradeep Singh <pdeepsingh91@gmail.com>
2020-05-03 10:36:27 -0700

    Create W1_2_jewels_and_stones.py

---
+++ leetcode-30day-challenge/May-2019/W1_2_jewels_and_stones.py
@@ -0,0 +1,19 @@
+'''
+# Prob: Jewels and Stones
+
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+'''
+
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0
+        for s in S:
+            if s in J:
+                count += 1
+        return count
