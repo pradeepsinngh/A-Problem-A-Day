ee27338a61a9d762b9b04f05dcbedc62f8ca7d67
Pradeep Singh <pdeepsingh91@gmail.com>
2020-05-03 11:25:44 -0700

    Update 003-game_of_life.py

--- by-data-structure/arrays/medium/003-game_of_life.py
+++ by-data-structure/arrays/medium/003-game_of_life.py
@@ -3,6 +3,10 @@
 -- https://leetcode.com/problems/game-of-life/
 '''
 
+# Sol 1: 
+# Space: O(m x n)
+# Time: O(m x n)
+
 
 class Solution(object):
     def gameOfLife(self, board):
@@ -38,8 +42,43 @@ class Solution(object):
                     board[row][col] = 1
             
                     
-                
+# Sol 2: 
+# Space: O(1)
+# Time: O(m x n)           
                     
-                        
+                
+class Solution:
+    def gameOfLife(self, board):
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        neighbors = [(1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1), (0,1), (1,1)]
+
+        rows = len(board)
+        cols = len(board[0])
+
+        for row in range(rows):
+            for col in range(cols):
+
+                live_neighbors = 0
+                for neighbor in neighbors:
+                    r = (row + neighbor[0])
+                    c = (col + neighbor[1])
+
+                    if (r < rows and r >= 0) and (c < cols and c >= 0) and abs(board[r][c]) == 1:
+                        live_neighbors += 1
+
+                if board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    board[row][col] = -1
+
+                if board[row][col] == 0 and live_neighbors == 3:
+                    board[row][col] = 2
+
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] > 0:
+                    board[row][col] = 1
+                else:
+                    board[row][col] = 0
                 
                 
